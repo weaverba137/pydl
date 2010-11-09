878d066936a9845a02d537256f6d4cf8a3220568
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2010-11-09 15:26:26 +0000

    Creating package directories.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='pydl',
+    version='0.1',
+    packages=['pydl', 'pydlutils', 'pydlspec2d', ],
+    license='GPLv3',
+    long_description=open('README.txt').read(),
+)
+
