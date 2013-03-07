2f02ef6d4e1e5d66cbdddb862d0a5bcb1b7dc6a7
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2013-03-07 16:01:15 -0500

    Update cooling functions

--- pydl/pydlutils/cooling/__init__.py
+++ pydl/pydlutils/cooling/__init__.py
@@ -1,1 +1,6 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+"""
+This subpackage corresponds to the cooling directory idlutils.
+"""
 from read_ds_cooling import read_ds_cooling
--- pydl/pydlutils/cooling/read_ds_cooling.py
+++ pydl/pydlutils/cooling/read_ds_cooling.py
@@ -1,21 +1,48 @@
-#
-#
-#
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
 def read_ds_cooling(fname,logT=None):
-    """Read in Dopita & Sutherland 1993 cooling function
+    """Read in Dopita & Sutherland 1993 cooling function.
+
+    Parameters
+    ----------
+    fname : { 'm-00.cie', 'm-05.cie', 'm+05.cie', 'm-10.cie', 'm-15.cie', 'm-20.cie', 'm-30.cie', 'mzero.cie' }
+        Name of the data file to read.
+    logT : array_like, optional
+        If provided, values will be interpolated to the provided values.  If
+        not provided, the values in the data files will be returned.
+
+    Returns
+    -------
+    read_ds_cooling : tuple
+        A tuple containing logT and loglambda, respectively.
+
+    Raises
+    ------
+    URLError
+        If the data file could not be retrieved.
+    ValueError
+        If the input file name is invalid.
+
+    Notes
+    -----
+    Retrieves data from http://www.mso.anu.edu.au/~ralph/data/cool/ rather
+    than storing data locally.
+
+    Examples
+    --------
+    >>> pydl.pydlutils.cooling.read_ds_cooling('m-15.cie')
     """
-    import os
-    import os.path
-    import numpy as np
-    from pydlutils import PydlutilsException
-    from pydlutils.goddard.misc import readcol
-    path = os.path.join(os.getenv('IDLUTILS_DIR'),'data','cooling',fname)
-    if os.path.exists(path):
-        logTin, nelec, nH, nt, loglambdanet, loglambdanorm = readcol(fname,skip=4)
-        if logT is None:
-            return (logTin, loglambdanet)
-        else:
-            loglambda = np.interp(logT,logTin,loglambdanet)
-            return (logT,loglambda)
+    from urllib2 import urlopen
+    from numpy import interp
+    from astropy.io import ascii
+    baseurl = 'http://www.mso.anu.edu.au/~ralph/data/cool/'
+    if fname not in ('m-00.cie', 'm-05.cie', 'm+05.cie', 'm-10.cie', 'm-15.cie', 'm-20.cie', 'm-30.cie', 'mzero.cie'):
+        raise ValueError('Invalid value for data file: {0}'.format(fname))
+    with urlopen(baseurl+fname) as coolingfile:
+        coolingfile = coolingfile.read()
+    data = ascii.read(coolingfile.split('\n')[2:],delimiter='\t')
+    if logT is None:
+        return (data['log(T)'].data, data['log(lambda net)'].data)
     else:
-        raise PydlutilsException("Could not find "+fname)
+        loglambda = interp(logT, data['log(T)'].data, data['log(lambda net)'].data)
+        return (logT,loglambda)
