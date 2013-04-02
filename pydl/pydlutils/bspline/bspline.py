d437e52893272489ad29e509bbf1cba947592e88
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2013-04-02 18:05:40 -0400

    change np import

--- pydl/pydlutils/bspline/bspline.py
+++ pydl/pydlutils/bspline/bspline.py
@@ -1,6 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 # -*- coding: utf-8 -*-
 from __future__ import print_function
+import numpy as np
 #
 class bspline(object):
     """Bspline class.
@@ -44,7 +45,7 @@ class bspline(object):
                 w = ((kwargs['placed'] >= startx) &
                     (kwargs['placed'] <= startx+rangex))
                 if w.sum() < 2:
-                    bkpt = self.np.arange(2,dtype='f')*rangex + startx
+                    bkpt = np.arange(2,dtype='f')*rangex + startx
                 else:
                     bkpt = kwargs['placed'][w]
             elif 'bkspace' in kwargs:
@@ -52,19 +53,19 @@ class bspline(object):
                 if nbkpts < 2:
                     nbkpts = 2
                 tempbkspace = rangex/float(nbkpts-1)
-                bkpt = self.np.arange(nbkpts,dtype='f')*tempbkspace + startx
+                bkpt = np.arange(nbkpts,dtype='f')*tempbkspace + startx
             elif 'nbkpts' in kwargs:
                 nbkpts = kwargs['nbkpts']
                 if nbkpts < 2:
                     nbkpts = 2
                 tempbkspace = rangex/float(nbkpts-1)
-                bkpt = self.np.arange(nbkpts,dtype='f')*tempbkspace + startx
+                bkpt = np.arange(nbkpts,dtype='f')*tempbkspace + startx
             elif 'everyn' in kwargs:
                 npkpts = max(nx/kwargs['everyn'], 1)
                 if nbkpts == 1:
                     xspot = [0]
                 else:
-                    xspot = int(nx/(nbkpts-1))*self.np.arange(nbkpts,dtype='i4')
+                    xspot = int(nx/(nbkpts-1))*np.arange(nbkpts,dtype='i4')
                 bkpt = x[xspot].astype('f')
             else:
                 raise ValueError('No information for bkpts.')
@@ -81,12 +82,12 @@ class bspline(object):
         nshortbkpt = bkpt.size
         fullbkpt = bkpt.copy()
         if nshortbkpt == 1:
-            bkspace = self.np.float32(bkspread)
+            bkspace = np.float32(bkspread)
         else:
-            bkspace = (bkpt[1] - bkpt[0]) * self.np.float32(bkspread)
-        for i in self.np.arange(1,nord,dtype=self.np.float32):
-            fullbkpt = self.np.insert(fullbkpt,0,bkpt[0]-bkspace*i)
-            fullbkpt = self.np.insert(fullbkpt,fullbkpt.shape[0],
+            bkspace = (bkpt[1] - bkpt[0]) * np.float32(bkspread)
+        for i in np.arange(1,nord,dtype=np.float32):
+            fullbkpt = np.insert(fullbkpt,0,bkpt[0]-bkspace*i)
+            fullbkpt = np.insert(fullbkpt,fullbkpt.shape[0],
                 bkpt[nshortbkpt-1] + bkspace*i)
         #
         # Set the attributes
@@ -95,13 +96,13 @@ class bspline(object):
         self.breakpoints = fullbkpt
         self.nord = nord
         self.npoly = npoly
-        self.mask = self.np.ones((fullbkpt.size,),dtype='bool')
+        self.mask = np.ones((fullbkpt.size,),dtype='bool')
         if npoly > 1:
-            self.coeff = self.np.zeros((npoly,nc),dtype='d')
-            self.icoeff = self.np.zeros((npoly,nc),dtype='d')
+            self.coeff = np.zeros((npoly,nc),dtype='d')
+            self.icoeff = np.zeros((npoly,nc),dtype='d')
         else:
-            self.coeff = self.np.zeros((nc,),dtype='d')
-            self.icoeff = self.np.zeros((nc,),dtype='d')
+            self.coeff = np.zeros((nc,),dtype='d')
+            self.icoeff = np.zeros((nc,),dtype='d')
         self.xmin = 0.0
         self.xmax = 1.0
         self.funcname = 'legendre'
@@ -126,27 +127,27 @@ class bspline(object):
         goodbk = self.mask[self.nord:]
         nn = goodbk.sum()
         if nn < self.nord:
-            yfit = self.np.zeros(ydata.shape,dtype='f')
+            yfit = np.zeros(ydata.shape,dtype='f')
             return (-2,yfit)
         nfull = nn * self.npoly
         bw = self.npoly * self.nord
         a1,lower,upper = self.action(xdata,x2=x2)
-        foo = self.np.tile(invvar,bw).reshape(bw,invvar.size).transpose()
+        foo = np.tile(invvar,bw).reshape(bw,invvar.size).transpose()
         a2 = a1 * foo
-        alpha = self.np.zeros((bw,nfull+bw),dtype='d')
-        beta = self.np.zeros((nfull+bw,),dtype='d')
-        bi = self.np.arange(bw,dtype='i4')
-        bo = self.np.arange(bw,dtype='i4')
+        alpha = np.zeros((bw,nfull+bw),dtype='d')
+        beta = np.zeros((nfull+bw,),dtype='d')
+        bi = np.arange(bw,dtype='i4')
+        bo = np.arange(bw,dtype='i4')
         for k in range(1,bw):
-            bi = self.np.append(bi, self.np.arange(bw-k,dtype='i4')+(bw+1)*k)
-            bo = self.np.append(bo, self.np.arange(bw-k,dtype='i4')+bw*k)
+            bi = np.append(bi, np.arange(bw-k,dtype='i4')+(bw+1)*k)
+            bo = np.append(bo, np.arange(bw-k,dtype='i4')+bw*k)
         for k in range(nn-self.nord+1):
             itop = k*self.npoly
             ibottom = min(itop,nfull) + bw - 1
             ict = upper[k] - lower[k] + 1
             if ict > 0:
-                work = self.np.dot(a1[lower[k]:upper[k]+1,:].T,a2[lower[k]:upper[k]+1,:])
-                wb = self.np.dot(ydata[lower[k]:upper[k]+1],a2[lower[k]:upper[k]+1,:])
+                work = np.dot(a1[lower[k]:upper[k]+1,:].T,a2[lower[k]:upper[k]+1,:])
+                wb = np.dot(ydata[lower[k]:upper[k]+1],a2[lower[k]:upper[k]+1,:])
                 alpha.T.flat[bo+itop*bw] += work.flat[bi]
                 beta[itop:ibottom+1] += wb
         min_influence = 1.0e-10 * invvar.sum() / nfull
@@ -163,11 +164,11 @@ class bspline(object):
             yfit,foo = self.value(xdata,x2=x2,action=a1,upper=upper,lower=lower)
             return (self.maskpoints(errs),yfit)
         if self.npoly > 1:
-            self.icoeff[:,goodbk] = self.np.array(a[0,0:nfull].reshape(self.npoly,nn),dtype=a.dtype)
-            self.coeff[:,goodbk] = self.np.array(sol[0:nfull].reshape(self.npoly,nn),dtype=sol.dtype)
+            self.icoeff[:,goodbk] = np.array(a[0,0:nfull].reshape(self.npoly,nn),dtype=a.dtype)
+            self.coeff[:,goodbk] = np.array(sol[0:nfull].reshape(self.npoly,nn),dtype=sol.dtype)
         else:
-            self.icoeff[goodbk] = self.np.array(a[0,0:nfull],dtype=a.dtype)
-            self.coeff[goodbk] = self.np.array(sol[0:nfull],dtype=sol.dtype)
+            self.icoeff[goodbk] = np.array(a[0,0:nfull],dtype=a.dtype)
+            self.coeff[goodbk] = np.array(sol[0:nfull],dtype=sol.dtype)
         yfit,foo = self.value(xdata,x2=x2,action=a1,upper=upper,lower=lower)
         return (0,yfit)
     #
@@ -193,26 +194,26 @@ class bspline(object):
         n = nbkpt - self.nord
         gb = self.breakpoints[self.mask]
         bw = self.npoly*self.nord
-        lower = self.np.zeros((n - self.nord + 1,),dtype='i4')
-        upper = self.np.zeros((n - self.nord + 1,),dtype='i4') -1
+        lower = np.zeros((n - self.nord + 1,),dtype='i4')
+        upper = np.zeros((n - self.nord + 1,),dtype='i4') -1
         indx = self.intrv(x)
         bf1 = self.bsplvn(x, indx)
         action = bf1
-        aa = uniq(indx,self.np.arange(indx.size,dtype='i4'))
+        aa = uniq(indx,np.arange(indx.size,dtype='i4'))
         upper[indx[aa]-self.nord+1] = aa
         rindx = indx[::-1]
-        bb = uniq(rindx,self.np.arange(rindx.size,dtype='i4'))
+        bb = uniq(rindx,np.arange(rindx.size,dtype='i4'))
         lower[rindx[bb]-self.nord+1] = nx - bb - 1
         if x2 is not None:
             if x2.size != nx:
                 raise ValueError('Dimensions of x and x2 do not match.')
             x2norm = 2.0 * (x2 - self.xmin) / (self.xmax - self.xmin ) - 1.0
             if self.funcname == 'poly':
-                temppoly = self.np.ones((nx,self.npoly),dtype='f')
+                temppoly = np.ones((nx,self.npoly),dtype='f')
                 for i in range(1,self.npoly):
                     temppoly[:,i] = temppoly[:,i-1] * x2norm
             elif self.funcname == 'poly1':
-                temppoly = self.np.tile(x2norm,self.npoly).reshape(nx,self.npoly)
+                temppoly = np.tile(x2norm,self.npoly).reshape(nx,self.npoly)
                 for i in range(1,self.npoly):
                     temppoly[:,i] = temppoly[:,i-1] * x2norm
             elif self.funcname == 'chebyshev':
@@ -221,7 +222,7 @@ class bspline(object):
                 temppoly = flegendre(x2norm,self.npoly)
             else:
                 raise ValueError('Unknown value of funcname.')
-            action = self.np.zeros((nx,bw),dtype='d')
+            action = np.zeros((nx,bw),dtype='d')
             counter = -1
             for ii in range(self.nord):
                 for jj in range(self.npoly):
@@ -243,7 +244,7 @@ class bspline(object):
         """
         gb = self.breakpoints[self.mask]
         n = gb.size - self.nord
-        indx = self.np.zeros((x.size,),dtype='i4')
+        indx = np.zeros((x.size,),dtype='i4')
         ileft = self.nord -1
         for i in range(x.size):
             while x[i] > gb[ileft+1] and ileft < n - 1:
@@ -265,7 +266,7 @@ class bspline(object):
         bsplvn
         """
         bkpt = self.breakpoints[self.mask]
-        vnikx = self.np.zeros((x.size,self.nord),dtype=x.dtype)
+        vnikx = np.zeros((x.size,self.nord),dtype=x.dtype)
         deltap = vnikx.copy()
         deltam = vnikx.copy()
         j = 0
@@ -311,9 +312,9 @@ class bspline(object):
                 raise ValueError('Must specify lower and upper if action is set.')
         else:
             action,lower,upper = self.action(xwork,x2=x2work)
-        yfit = self.np.zeros(x.shape,dtype=x.dtype)
+        yfit = np.zeros(x.shape,dtype=x.dtype)
         bw = self.npoly * self.nord
-        spot = self.np.arange(bw,dtype='i4')
+        spot = np.arange(bw,dtype='i4')
         goodbk = self.mask.nonzero()[0]
         coeffbk = self.mask[self.nord:].nonzero()[0]
         n = self.mask.sum() - self.nord
@@ -321,21 +322,21 @@ class bspline(object):
             goodcoeff = self.coeff[:,coeffbk]
         else:
             goodcoeff = self.coeff[coeffbk]
-        # maskthis = self.np.zeros(xwork.shape,dtype=xwork.dtype)
+        # maskthis = np.zeros(xwork.shape,dtype=xwork.dtype)
         for i in range(n-self.nord+1):
             ict = upper[i] - lower[i] + 1
             if ict > 0:
-                yfit[lower[i]:upper[i]+1] = self.np.dot(
+                yfit[lower[i]:upper[i]+1] = np.dot(
                     action[lower[i]:upper[i]+1,:],goodcoeff[i*self.npoly+spot])
         yy = yfit.copy()
         yy[xsort] = yfit
-        mask = self.np.ones(x.shape,dtype='bool')
+        mask = np.ones(x.shape,dtype='bool')
         gb = self.breakpoints[goodbk]
         outside = ((x < gb[self.nord-1]) |
             (x > gb[n]))
         if outside.any():
             mask[outside] = False
-        hmm = ((self.np.diff(goodbk) > 2).nonzero())[0]
+        hmm = ((np.diff(goodbk) > 2).nonzero())[0]
         for jj in range(hmm.size):
             inside = ((x >= self.breakpoints[goodbk[hmm[jj]]]) &
                 (x <= self.breakpoints[goodbk[hmm[jj]+1]-1]))
@@ -358,14 +359,14 @@ class bspline(object):
         nbkpt = self.mask.sum()
         if nbkpt <= 2*self.nord:
             return -2
-        hmm = err[self.np.unique(errb/self.npoly)]/self.npoly
+        hmm = err[np.unique(errb/self.npoly)]/self.npoly
         n = nbkpt - self.nord
-        if self.np.any(hmm >= n):
+        if np.any(hmm >= n):
             return -2
-        test = self.np.zeros(nbkpt,dtype='bool')
-        for jj in range(-self.np.ceil(nord/2.0),nord/2.0):
-            foo = self.np.where((hmm+jj) > 0,hmm+jj,self.np.zeros(hmm.shape,dtype=hmm.dtype))
-            inside = self.np.where((foo+nord) < n-1,foo+nord,self.np.zeros(hmm.shape,dtype=hmm.dtype)+n-1)
+        test = np.zeros(nbkpt,dtype='bool')
+        for jj in range(-np.ceil(nord/2.0),nord/2.0):
+            foo = np.where((hmm+jj) > 0,hmm+jj,np.zeros(hmm.shape,dtype=hmm.dtype))
+            inside = np.where((foo+nord) < n-1,foo+nord,np.zeros(hmm.shape,dtype=hmm.dtype)+n-1)
             test[inside] = True
         if test.any():
             reality = self.mask[test]
