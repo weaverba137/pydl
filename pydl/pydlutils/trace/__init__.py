85c20566696a8016310d2c129b6ea92725863aa1
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2015-06-22 18:15:37 -0400

    preparing trace directory

---
+++ pydl/pydlutils/trace/__init__.py
@@ -0,0 +1,6 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+"""
+This subpackage corresponds to the trace directory of idlutils.
+"""
+from .fchebyshev import fchebyshev
