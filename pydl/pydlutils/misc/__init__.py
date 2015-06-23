0f690dee220e000405822d709cdc4a6ac25d2fc3
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2015-06-23 16:17:47 -0400

    adding djs_laxisgen

--- pydl/pydlutils/misc/__init__.py
+++ pydl/pydlutils/misc/__init__.py
@@ -4,7 +4,8 @@
 This subpackage corresponds to the misc directory of idlutils.
 """
 from .decode_mixed import decode_mixed
+from .djs_laxisgen import djs_laxisgen
 from .djs_laxisnum import djs_laxisnum
-#from .djs_readcol import djs_readcol
+# from .djs_readcol import djs_readcol
 from .hogg_iau_name import hogg_iau_name
 from .struct_print import struct_print
---
+++ pydl/pydlutils/misc/djs_laxisgen.py
@@ -0,0 +1,62 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+def djs_laxisgen(dims,iaxis=0):
+    """Returns an integer array where each element of the array is set equal to its index number along the specified axis.
+
+    Parameters
+    ----------
+    dims : list
+        Dimensions of the array to return.
+    iaxis : int, optional
+        Index along this dimension.
+
+    Returns
+    -------
+    djs_laxisgen : ndarray
+        An array of indexes with ``dtype=int32``.
+
+    Raises
+    ------
+    ValueError
+        If `iaxis` is greater than or equal to the number of dimensions.
+    NotImplementedError
+        If the number of dimensions is greater than three.
+
+    Examples
+    --------
+    >>> from pydl.pydlutils.misc import djs_laxisgen
+    >>> djs_laxisgen([4,4])
+    array([[0, 0, 0, 0],
+           [1, 1, 1, 1],
+           [2, 2, 2, 2],
+           [3, 3, 3, 3]], dtype=int32)
+    """
+    from numpy import zeros, arange
+    ndimen = len(dims)
+    result = zeros(dims,dtype='i4')
+    if ndimen == 1:
+        return arange(dims[0],dtype='i4')
+    elif ndimen == 2:
+        if iaxis == 0:
+            for k in range(dims[0]):
+                result[k,:] = k
+        elif iaxis == 1:
+            for k in range(dims[1]):
+                result[:,k] = k
+        else:
+            raise ValueError("Bad value for iaxis: {0:d}".format(iaxis))
+    elif ndimen == 3:
+        if iaxis == 0:
+            for k in range(dims[0]):
+                result[k,:,:] = k
+        elif iaxis == 1:
+            for k in range(dims[1]):
+                result[:,k,:] = k
+        elif iaxis == 2:
+            for k in range(dims[2]):
+                result[:,:,k] = k
+        else:
+            raise ValueError("Bad value for iaxis: {0:d}".format(iaxis))
+    else:
+        raise NotImplementedError("{0:d} dimensions not supported.".format(ndimen))
+    return result
