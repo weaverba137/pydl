6a0533375495d7614464b05d456f4ad79113846c
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2015-07-09 15:48:36 -0400

    add tests of write_ndarray_to_yanny

--- pydl/pydlutils/yanny/tests/test_write_ndarray_to_yanny.py
+++ pydl/pydlutils/yanny/tests/test_write_ndarray_to_yanny.py
@@ -5,7 +5,9 @@ import os
 import numpy as np
 from .. import yanny
 from .. import write_ndarray_to_yanny
+from ... import PydlutilsException
 from . import YannyTestCase
+from astropy.tests.helper import raises
 
 class TestWriteNdarray(YannyTestCase):
     """Test class for ndarray to yanny conversions."""
@@ -54,3 +56,42 @@ class TestWriteNdarray(YannyTestCase):
         assert 'typedef struct {\n    long timestamp;\n    STATUS state;\n} MY_STATUS;' in par['symbols']['struct']
         for k,f in enumerate(('FALSE','TRUE','TRUE','FALSE')):
             assert par['MAGNITUDES']['new_flag'][k].decode() == f
+
+    def test_write_ndarray_to_yanny_exceptions(self):
+        """Make sure certain execptions are raised by the write_ndarray_to_yanny function."""
+        mystruct = np.zeros((4,),
+            dtype=[('ra','f8'), ('dec','f8'), ('mag','f4',(5,)),
+                ('flags','i4'), ('new_flag','|S5')])
+        mystruct['ra'] = np.array([10.0, 20.5, 30.75, 40.55],dtype=np.float64)
+        mystruct['dec'] = np.array([-5.1234, -10.74832, 67.994523, 11.437281],dtype=np.float64)
+        mystruct['mag'] = np.array([[0.0,1.0,2.0,3.0,4.0],
+            [5.1,6.2,7.3,8.4,9.5],
+            [22.123,23.95,22.6657,21.0286,22.9876],
+            [13.54126,15.37456,14.52647,12.648640,12.0218]],dtype=np.float32)
+        mystruct['flags'] = np.array([2**2, 2**4, 2**6, 2**8 + 2**3],dtype=np.int32)
+        mystruct['new_flag'] = np.array(['FALSE','TRUE','TRUE','FALSE'],dtype='|S5')
+        enums = {'new_flag':('BOOLEAN',('FALSE','TRUE'))}
+        with raises(PydlutilsException):
+            par = write_ndarray_to_yanny(os.path.join(self.data_dir,'test.par'),mystruct,structnames='magnitudes',enums=enums)
+        with raises(PydlutilsException):
+            par = write_ndarray_to_yanny(self.temp('tempfile3.par'),mystruct,structnames=('magnitudes','my_status'),enums=enums)
+
+    def test_write_ndarray_to_yanny_misc(self):
+        """Test other minor features of write_ndarray_to_yanny."""
+        mystruct = np.zeros((4,),
+            dtype=[('ra','f8'), ('dec','f8'), ('mag','f4',(5,)),
+                ('flags','i4'), ('new_flag','|S5')])
+        mystruct['ra'] = np.array([10.0, 20.5, 30.75, 40.55],dtype=np.float64)
+        mystruct['dec'] = np.array([-5.1234, -10.74832, 67.994523, 11.437281],dtype=np.float64)
+        mystruct['mag'] = np.array([[0.0,1.0,2.0,3.0,4.0],
+            [5.1,6.2,7.3,8.4,9.5],
+            [22.123,23.95,22.6657,21.0286,22.9876],
+            [13.54126,15.37456,14.52647,12.648640,12.0218]],dtype=np.float32)
+        mystruct['flags'] = np.array([2**2, 2**4, 2**6, 2**8 + 2**3],dtype=np.int32)
+        mystruct['new_flag'] = np.array(['FALSE','TRUE','TRUE','FALSE'],dtype='|S5')
+        enums = {'new_flag':('BOOLEAN',('FALSE','TRUE'))}
+        hdr = {'keyword1':'value1','keyword2':'value2'}
+        par = write_ndarray_to_yanny(self.temp('tempfile1.par'),mystruct,hdr=hdr,enums=enums)
+        assert 'MYSTRUCT0' in par
+        assert par['keyword1'] == hdr['keyword1']
+        assert par['keyword2'] == hdr['keyword2']
