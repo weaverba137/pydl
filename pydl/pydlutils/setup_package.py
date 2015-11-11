dcfaad84a4a1c1dfabeb1e947895e6651ea602d7
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2015-11-10 20:04:32 -0500

    rearrange yanny package

--- pydl/pydlutils/setup_package.py
+++ pydl/pydlutils/setup_package.py
@@ -4,5 +4,5 @@ def get_package_data():
     # Installs the testing data files.  Unable to get package_data
     # to deal with a directory hierarchy of files, so just explicitly list.
     return {
-        'pydl.pydlutils.yanny.tests': ['t/*',]
+        'pydl.pydlutils.tests': ['t/*',]
     }
--- pydl/pydlutils/yanny/__init__.py
+++
@@ -1,6 +0,0 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
-# -*- coding: utf-8 -*-
-"""
-This subpackage corresponds to the yanny directory of idlutils.
-"""
-from .yanny import write_ndarray_to_yanny, yanny
