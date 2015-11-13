65fcde322b746316d7fe7f7da7274c50b7f28edf
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2015-11-13 16:29:54 -0500

    consolidate sdss code

--- pydl/pydlutils/sdss/tests/test_default_skyversion.py
+++
@@ -1,7 +0,0 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
-# -*- coding: utf-8 -*-
-
-
-def test_default_skyversion():
-    from .. import default_skyversion
-    assert default_skyversion() == 2
