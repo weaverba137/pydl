54a1bf9ac5e6eb56bd4d7b2f11b4df50d1e21fce
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2015-12-09 13:20:36 -0500

    clean up imports

--- pydl/pydlspec2d/spec1d/__init__.py
+++ pydl/pydlspec2d/spec1d/__init__.py
@@ -4,13 +4,10 @@
 This subpackage corresponds to the spec1d directory of idlspec2d.
 """
 from .findspec import findspec
-from .hmf_gal import hmf_gal
 from .hmf_solve import hmf_solve
 from .latest_mjd import latest_mjd
 from .number_of_fibers import number_of_fibers
-from .pca_qso import pca_qso
 from .pca_solve import pca_solve
-from .pca_star import pca_star
 from .plot_eig import plot_eig
 from .readspec import readspec
 from .skymask import skymask
