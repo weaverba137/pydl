bc6bf3d5071d078f0e62b0d0ca5834834335c2cb
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2016-03-13 00:12:51 -0500

    remove old tests

--- pydl/pydlspec2d/spec1d/tests/test_findspec.py
+++
@@ -1,9 +0,0 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
-# -*- coding: utf-8 -*-
-
-
-def test_findspec():
-    """This is just a placeholder for now."""
-    from .. import findspec
-    # slist = findspec(infile='file.in', sdss=True)
-    assert True
