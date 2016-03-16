adad5fa81a5f05d06e1e74574ab961f7e7957bc8
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2016-03-16 15:19:12 -0400

    add test_cooling.py

---
+++ pydl/pydlutils/tests/test_cooling.py
@@ -0,0 +1,24 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+import numpy as np
+from astropy.tests.helper import raises
+from ..cooling import read_ds_cooling
+
+
+class TestCooling(object):
+    """Test the functions in pydl.pydlutils.cooling.
+    """
+
+    def setup(self):
+        pass
+
+    def teardown(self):
+        pass
+
+    def test_read_ds_cooling(self):
+        with raises(ValueError):
+            logT, loglambda = read_ds_cooling('m-99.cie')
+        logT, logL = read_ds_cooling('m-15.cie')
+        assert np.allclose(logT[0:5], np.array([4.0, 4.05, 4.1, 4.15, 4.2])
+        assert np.allclose(logL[0:5], np.array([-26.0, -24.66, -23.52,
+                                                -22.62, -22.11])
