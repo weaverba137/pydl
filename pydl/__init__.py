c8f1ee5c7c6f01a524a2fab5676949fcd0d516e0
Benjamin Alan Weaver <baweaver@lbl.gov>
2018-05-02 14:37:14 -0400

    update based on latest package-template rendered branch

--- pydl/__init__.py
+++ pydl/__init__.py
@@ -13,14 +13,24 @@ functions are implemented to support this.
 .. _IDL: http://www.exelisvis.com/language/en-us/productsservices/idl.aspx
 """
 
-# Affiliated packages may add whatever they like to this file, but
+# Packages may add whatever they like to this file, but
 # should keep this content at the top.
 # ----------------------------------------------------------------------------
 from ._astropy_init import *
 # ----------------------------------------------------------------------------
 
-# For egg_info test builds to pass, put package imports here.
+# Enforce Python version check during package import.
+# This is the same check as the one at the top of setup.py
+import sys
+
+class UnsupportedPythonError(Exception):
+    pass
+
+if sys.version_info < tuple((int(val) for val in "2.7".split('.'))):
+    raise UnsupportedPythonError("packagename does not support Python < {}".format(2.7))
+
 if not _ASTROPY_SETUP_:
+    # For egg_info test builds to pass, put package imports here.
     from .file_lines import file_lines
     from .median import median
     from .pcomp import pcomp
