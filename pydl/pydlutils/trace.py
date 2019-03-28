6bd99175442e022897e849fcbd713c3808b0e0d3
Benjamin Alan Weaver <baweaver@lbl.gov>
2019-03-28 09:42:34 -0600

    add traceset definition

--- pydl/pydlutils/trace.py
+++ pydl/pydlutils/trace.py
@@ -1,6 +1,26 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 # -*- coding: utf-8 -*-
 """This module corresponds to the trace directory in idlutils.
+
+Traceset (or "trace set"):
+    A set of coefficient vectors defining a set of functions
+    over a common independent-variable domain specified by ``xmin``
+    and ``xmax`` values. The functions in the set are defined in
+    terms of a linear combination of basis functions (such as
+    Legendre of Chebyshev polynonials) up to a specified maximum
+    order, weighted by the values in the coefficient vectors, and
+    evaluated using a suitable affine rescaling of the
+    1dependent-variable domain (such as ``[xmin, xmax] -> [-1, 1]``
+    for Legendre polynomials). For evaluation purposes, the
+    domain is assumed by default to be a zero-based integer
+    baseline from ``xmin`` to ``xmax`` such as would correspond to a
+    digital detector pixel grid.
+
+Etymology:
+    From the original motivating use case of encoding the "traces"
+    of multiple spectra across the detector in a multi-fiber spectrograph.
+
+Definition from A. Bolton, U. of Utah, June 2015.
 """
 import numpy as np
 from scipy.special import chebyt
