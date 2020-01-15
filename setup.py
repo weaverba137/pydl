19cdae5290954c39830f89c73452ef2b94af66c6
Benjamin Alan Weaver <baweaver@lbl.gov>
2020-01-15 11:20:27 -0700

    remove astropy_helpers from setup.py

--- setup.py
+++ setup.py
@@ -2,26 +2,58 @@
 # -*- coding: utf-8 -*-
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-# Needed to import local helpers when pyproject.toml exists.
+import os
 import sys
-sys.path.append(".")
-
-# Ensure that astropy-helpers is available
-import ah_bootstrap  # noqa
-
+# Needed to import local helpers when pyproject.toml exists.
+# sys.path.append(".")
 from setuptools import setup
+from distutils.command.sdist import sdist as DistutilsSdist
 
-from astropy_helpers.setup_helpers import register_commands
-# from distutils.command.sdist import sdist as DistutilsSdist
+TEST_HELP = """
+Note: running tests is no longer done using 'python setup.py test'. Instead
+you will need to run:
 
-# Create a dictionary with setup command overrides. Note that this gets
-# information about the package (name and version) from the setup.cfg file.
-cmdclass = register_commands()
-# cmdclass = {'sdist': DistutilsSdist}
-cmdclass.pop('build_sphinx', None)
-cmdclass.pop('build_docs', None)
-# cmdclass.pop('sdist', None)
-cmdclass.pop('build_ext', None)
-# print(cmdclass)
+    tox -e test
 
-setup(use_scm_version={"write_to": "pydl/version.py"}, cmdclass=cmdclass)
+If you don't already have tox installed, you can install it with:
+
+    pip install tox
+
+If you only want to run part of the test suite, you can also use pytest
+directly with::
+
+    pip install -e .
+    pytest
+
+For more information, see:
+
+  http://docs.astropy.org/en/latest/development/testguide.html#running-tests
+"""
+
+if 'test' in sys.argv:
+    print(TEST_HELP)
+    sys.exit(1)
+
+DOCS_HELP = """
+Note: building the documentation is no longer done using
+'python setup.py build_docs'. Instead you will need to run:
+
+    tox -e build_docs
+
+If you don't already have tox installed, you can install it with:
+
+    pip install tox
+
+For more information, see:
+
+  http://docs.astropy.org/en/latest/install.html#builddocs
+"""
+
+if 'build_docs' in sys.argv or 'build_sphinx' in sys.argv:
+    print(DOCS_HELP)
+    sys.exit(1)
+
+cmdclass = {'sdist': DistutilsSdist}
+
+setup(use_scm_version={"write_to": os.path.join("pydl", "version.py")},
+      cmdclass=cmdclass)
