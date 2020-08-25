4a1c36962eac987f33e49906e18377c5afb545d9
Benjamin Alan Weaver <baweaver@lbl.gov>
2020-08-25 12:33:06 -0700

    remove extraneous str

--- pydl/pydlutils/yanny.py
+++ pydl/pydlutils/yanny.py
@@ -1,4 +1,4 @@
-(str,)# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
 # -*- coding: utf-8 -*-
 """This module corresponds to the yanny directory in idlutils.
 
